7a57917fa23ddcf1b40daf2a93fa0193947baab3
Summer <511391805@qq.com>
2022-03-29 21:38:32 +0800

    :fire:搭建 eureka-server

---
+++ eureka-server/src/main/java/com/summer/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.summer.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Summer
+ * @date 2022/3/29 21:28
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
