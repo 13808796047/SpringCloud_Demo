b3a8a41573af90c94fbbbbf0545cf6b751059f1f
Summer <511391805@qq.com>
2022-03-29 23:33:40 +0800

    :rocket:Nacos服务注册

--- order-service/src/main/java/com/summer/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/summer/order/OrderServiceApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @MapperScan("com.summer.order.mapper")
-@EnableEurekaClient
+//@EnableEurekaClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- user-service/src/main/java/com/summer/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/summer/userservice/UserServiceApplication.java
@@ -3,7 +3,6 @@ package com.summer.userservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Summer
@@ -11,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @MapperScan("com.summer.userservice.mapper")
-@EnableEurekaClient
+//@EnableEurekaClient
 public class UserServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class,args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }
